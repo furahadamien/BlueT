ccb68c2dcd25bbb7a7f8eebbd93f97219a24304a
furahadamien <furahadamien30@gmail.com>
2020-07-28 13:03:38 -0400

    modified tracker

--- runner.js
+++ runner.js
@@ -1,3 +1,4 @@
 const path = require('path');
-const bleTracker = require('./lib/bluet')
-module.exports = bleTracker.ScanDevices();
+const blueTracker = require('./lib/bluet')
+module.exports = blueTracker.ScanDevices();
+module.exports = blueTracker.TrackDevice();
