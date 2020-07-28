01e6aecaa42e3a58052fcda9e72af4c468642f99
furahadamien <furahadamien30@gmail.com>
2020-07-28 13:22:33 -0400

    catch uuid

--- runner.js
+++ runner.js
@@ -1,4 +1,7 @@
 const path = require('path');
 const blueTracker = require('./lib/bluet')
-module.exports = blueTracker.ScanDevices();
-module.exports = blueTracker.TrackDevice();
+//blueTracker.ScanDevices(5)
+blueTracker.TrackDevice('e43936e2c80c446ca4ba762b6d8c565f');
+
+module.exports = numOfDevices => blueTracker.ScanDevices(numOfDevices);
+module.exports = DEVICEUUID => blueTracker.TrackDevice(DEVICEUUID);
